02b6943a979ce2474cde894a5a1624b1fe67be4b
khinsuwai97 <khinsuwai009@gmail.com>
2023-07-03 13:52:58 +0630

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import useQuizContext from './store/useQuizContext';
+import QuizForm from './components/QuizForm';
+import Loading from './components/Loading';
+import Quiz from './components/Quiz';
+
+const App = () => {
+  const { isLoading, startQuiz, error } = useQuizContext();
+  if (startQuiz) {
+    return <QuizForm />;
+  }
+  if (error) {
+    return <h3>{error}</h3>;
+  }
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <>
+      <Quiz />
+    </>
+  );
+};
+
+export default App;
